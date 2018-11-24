5c36b3f2a8134ddd3802b94b61745176a21888a8
will.stankus <william.r.stankus@wmich.edu>
2018-11-23 23:17:27 -0600

    Added php

---
+++ data.php
@@ -0,0 +1,14 @@
+<?php
+
+$items = array();
+$items[0] = array("title" => "Brown Recliner", "description" => "blah blah blah blah");
+$items[1] = array("title" => "Brown Recliner");
+$items[2] = array("title" => "Brown Recliner");
+$items[3] = array("title" => "Brown Recliner");
+$items[4] = array("title" => "Brown Recliner");
+$items[5] = array("title" => "Brown Recliner");
+$items[6] = array("title" => "Brown Recliner");
+$items[7] = array("title" => "Brown Recliner");
+$items[8] = array("title" => "Brown Recliner");
+
+?>
