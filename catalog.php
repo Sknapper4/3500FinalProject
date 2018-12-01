3d587cd11c9444b651c7c918d6604c8593e100f6
sknapper4 <knapperstephen@gmail.com>
2018-12-01 16:56:35 -0500

    Started new user registration

--- catalog.php
+++ catalog.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
 
 
@@ -20,7 +20,7 @@
                 <div class="navbar-nav nav-tabs bg-transparent border-bottom-0">
                     <a class="nav-item nav-link active bg-transparent border-0" href="index.html">Home <span
                                 class="sr-only">(current)</span></a>
-                    <a class="nav-item nav-link bg-transparent" href="catalog.html">Shop</a>
+                    <a class="nav-item nav-link bg-transparent" href="catalog.php">Shop</a>
                     <a class="nav-item nav-link bg-transparent" href="about_us.html">About Us</a>
                     <a class="nav-item nav-link bg-transparent" href="origin_story.html">Origins</a>
                     <a class="nav-item nav-link bg-transparent" href="reviews.html">Reviews</a>
@@ -30,82 +30,80 @@
         </nav>
     </div>
 </header>
-<body class="">
-<main class="">
+<body>
     <div class="row origin-bg">
         <div class="col-2">
-        <div class="card p-0 m-2 mt-2">
-            <div class= "card-header bg-secondary text-white text-center">Type</div>
-            <ul class="list-inline">
-                <li class="p-2 ml-0 text-center">
-                    <a href="catalog.php?type=Recliner" class="text-dark">Recliners</a>
-                </li>
-                <li class="p-2 text-center">
-                    <a class="text-dark" href="catalog.php?type=Couch">Couches</a>
-                </li>
-                <li class="p-2 text-center">
-                    <a class="text-dark" href="catalog.php?type=Table">Tables</a>
-                </li>
-                <li class="p-2 text-center">
-                    <a class="text-dark" href="catalog.php?type=Mattress">Mattresses</a>
-                </li>
-                <li class="p-2 text-center">
-                    <a class="text-dark" href="catalog.php">All</a>
-                </li>
-            </ul>
-        </div>
+            <div class="card p-0 m-2 mt-2">
+                <div class="card-header bg-secondary text-white text-center">Type</div>
+                <ul class="list-inline">
+                    <li class="p-2 ml-0 text-center">
+                        <a href="catalog.php?type=Recliner" class="text-dark">Recliners</a>
+                    </li>
+                    <li class="p-2 text-center">
+                        <a class="text-dark" href="catalog.php?type=Couch">Couches</a>
+                    </li>
+                    <li class="p-2 text-center">
+                        <a class="text-dark" href="catalog.php?type=Table">Tables</a>
+                    </li>
+                    <li class="p-2 text-center">
+                        <a class="text-dark" href="catalog.php?type=Mattress">Mattresses</a>
+                    </li>
+                    <li class="p-2 text-center">
+                        <a class="text-dark" href="catalog.php">All</a>
+                    </li>
+                </ul>
+            </div>
         </div>
         <div class="col-10 pre-scrollable">
 
             <?php
             include 'data.php';
-            if (isset($_GET['type'])){
+            if (isset($_GET['type'])) {
                 $type = $_GET['type'];
                 foreach ($items as $val) {
                     if ($val["type"] == $type) {
-                        echo '<div class="row bg-white mt-2 rounded border border-info">
-                <div class="col-2">
-                    <img src="' . $val["pic"] . '" height=150>
-                </div>
-                <div class="col-8 bg-light">
-                    <a href="product.php?id=' . $val[" id"] . '"><strong>' . $val["title"] . '</strong></a><br><small>' . $val["company"] . '</small>
-                    <div class="mt-2">
-                        ' . $val["description"] . '
-                    </div>
-                </div>
-                <div class="col-2 bg-light text-right">
-                    <div>
-                        In Stock: ' . $val["stock"] . '
-                    </div>
-                    <div class="mt-3">
-                        <strong>&curren;' . $val["price"] . '</strong>
-                    </div>
-                </div>
-            </div>';
+                        echo '<div class="row bg-white mt-5 rounded border border-info">
+                                <div class="col-2">
+                                    <img src="' .  $val["pic"] . '" height=150 alt="'  . $val["title"] . '">
+                                </div>
+                                <div class="col-8 bg-light">
+                                    <a class="btn btn-info" href="product.php?id=' . $val[" id"] . '"><strong>' . $val["title"] . '</strong></a><br><small>' . $val["company"] . '</small>
+                                    <div class="mt-2">' . $val["description"] . '</div>
+                                </div>
+                                <div class="col-2 bg-light text-right">
+                                    <div>
+                                        <h6>In Stock: ' . $val["stock"] . '</h6>
+                                    </div>
+                                    <div class="mt-3">
+                                        <strong>&curren;' . $val["price"] . '</strong>
+                                    </div>
+                                </div>
+                            </div>';
+                    } else {
                     }
                 }
-            }
-            else{
+                unset($val);
+            } else {
                 foreach ($items as $val) {
-                    echo '<div class="row bg-white mt-2 rounded border border-info">
-                <div class="col-2">
-                    <img src="'.$val["pic"].'" height=150>
-                </div>
-                <div class="col-8 bg-light">
-                    <a href="product.php?id='.$val[" id"].'"><strong>'.$val["title"].'</strong></a><br><small>'.$val["company"].'</small>
-                    <div class="mt-2">
-                        '.$val["description"].'
-                    </div>
-                </div>
-                <div class="col-2 bg-light text-right">
-                    <div>
-                        In Stock: '. $val["stock"].'
-                    </div>
-                    <div class="mt-3">
-                        <strong>&curren;'. $val["price"].'</strong>
-                    </div>
-                </div>
-            </div>';
+                    echo '<div class="row bg-white mt-5 rounded border border-info">
+                            <div class="col-2">
+                                <img src="' . $val["pic"] . ' " height=150>
+                            </div>
+                            <div class="col-8 bg-light">
+                                <a href="product.php?id=' . $val["id"] . '"><strong>' . $val["title"] . '</strong></a><br><small>' . $val["company"] . '</small>
+                                <div class="mt-2">
+                                    ' . $val["description"] . '
+                                </div>
+                            </div>
+                            <div class="col-2 bg-light text-right">
+                                <div>
+                                    <h6>In Stock: ' . $val["stock"] . '</h6>
+                                </div>
+                                <div class="mt-3">
+                                    <strong>&curren;' . $val["price"] . '</strong>
+                                </div>
+                            </div>
+                        </div>';
                 }
             }
             ?>
@@ -114,12 +112,14 @@
         </div>
     </div>
 
-
-
-</main>
-
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
 </body>
 </html>
---
+++ registration/errors.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: skhome
+ * Date: 2018-12-01
+ * Time: 16:26
+ */
