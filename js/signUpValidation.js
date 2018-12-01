7eece09e98e41d48771f8f72c3f1a1eb81749a86
sknapper4 <knapperstephen@gmail.com>
2018-12-01 13:26:18 -0500

    Finished password validation

--- js/signUpValidation.js
+++ js/signUpValidation.js
@@ -1,34 +1,35 @@
-// $(document).ready(function () {
-//     var $inputClass = $('.inputClass');
-//
-//     $hilightableClass.on('focus', function () {
-//         $(this).toggleClass('highlight');
-//     });
-//
-//     $hilightableClass.on('blur', function () {
-//         $(this).toggleClass('highlight');
-//     });
-//
-//     $(':submit').click(function (e) {
-//         $('.required').each(function () {
-//             if ($(this).val().length == 0) {
-//                 $(this).addClass('error');
-//             }
-//         });
-//
-//         $('.error').keyup(function () {
-//             if ($(this).val().length > 0) {
-//                 $(this).removeClass('error');
-//             }
-//         });
-//
-//         e.preventDefault();
-//     });
-// });
 
+
+function checkPasswordMatch() {
+    var password = $("#password").val();
+    var confirmPassword = $("#confirmPassword").val();
+
+    if (password != confirmPassword)
+        $("#divCheckPasswordMatch").html("Passwords do not match!");
+    else
+        $("#divCheckPasswordMatch").html("Passwords match.");
+}
+
+$(document).ready(function () {
+
+});
 
 $(document).ready(function () {
     $(':submit').click(function (e) {
+        var password = $("#password").val();
+        var confirmPassword = $("#confirmPassword").val();
+        if (password.length < 6)
+            $("#divCheckPasswordMatch").html("Password must be at least 6 characters.");
+        else
+            if (password != confirmPassword)
+                $("#divCheckPasswordMatch").html("Passwords do not match!");
+            else
+                $("#divCheckPasswordMatch").html("Passwords match!");
+
+        $('.password').keyup(function () {
+            $(this).onchange($("#divCheckPasswordMatch").html(""));
+        })
+
         $('.required').each(function () {
             if ($(this).val().length == 0) {
                 $(this).addClass('error');
@@ -43,4 +44,5 @@ $(document).ready(function () {
 
         e.preventDefault();
     });
+
 })
